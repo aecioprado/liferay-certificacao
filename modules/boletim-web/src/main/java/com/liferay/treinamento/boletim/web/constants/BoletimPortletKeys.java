3f3981dc0d134614dff55fc222e70a423d07bf41
Aecio Prado <aecioprado@gmail.com>
2023-09-19 18:04:44 -0300

    feat: criacao do modulo mvc-portlet boletim-web

---
+++ modules/boletim-web/src/main/java/com/liferay/treinamento/boletim/web/constants/BoletimPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.treinamento.boletim.web.constants;
+
+/**
+ * @author vertigo
+ */
+public class BoletimPortletKeys {
+
+	public static final String BOLETIM =
+		"com_liferay_treinamento_boletim_web_BoletimPortlet";
+
+}
---
+++ modules/boletim-web/src/main/java/com/liferay/treinamento/boletim/web/portlet/BoletimPortlet.java
@@ -0,0 +1,29 @@
+package com.liferay.treinamento.boletim.web.portlet;
+
+import com.liferay.treinamento.boletim.web.constants.BoletimPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author vertigo
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Boletim",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + BoletimPortletKeys.BOLETIM,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class BoletimPortlet extends MVCPortlet {
+}
