42d0e031ff9b4559ae8cb33433628c330587204a
Aecio Prado <aecioprado@gmail.com>
2023-09-20 13:45:33 -0300

    feat: criacao de constantes para MVCCommandConstants

---
+++ modules/boletim-web/src/main/java/com/liferay/treinamento/boletim/web/constants/MVCCommandConstants.java
@@ -0,0 +1,18 @@
+package com.liferay.treinamento.boletim.web.constants;
+
+public class MVCCommandConstants {
+
+    // MVCCommands apontam para os controllers específicos
+    public static final String ADD_TAREFA = "/boletim/tarefa/add";
+    public static final String DELETE_TAREFA = "/boletim/tarefa/delete";
+    public static final String EDIT_TAREFA = "/boletim/tarefa/edit";
+    public static final String VIEW_TAREFA = "/boletim/tarefa/view";
+    public static final String VIEW_TAREFAS = "/boletim/tarefas/view";
+
+    // These are used for the optional exercise "Implement Submissions".
+    public static final String ADD_ENVIO = "/boletim/envio/add";
+    public static final String DELETE_ENVIO = "/boletim/envio/delete";
+    public static final String EDIT_ENVIO = "/boletim/envio/edit";
+    public static final String GRADE_ENVIO = "/boletim/envio/grade";
+    public static final String VIEW_ENVIO = "/boletim/envio/view";
+}
